87ef44fd8e9ce166ece990426579d20afc685521
Iris van Willigen <iris.van.willigen@gmail.com>
2024-01-09 07:56:30 +0100

    add hover effect that leaves pixelated trail

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 
 function generateGrid(size) {
-    for (i = 1; i <= (size*size); i++) {
-        newPixelDimension = 100/size;
+    for (i = 1; i <= (size * size); i++) {
+        newPixelDimension = 100 / size;
         newPixel = document.createElement('div');
         newPixel.setAttribute('id', ('div' + i));
-        newPixel.setAttribute('style', ('div' + i));
         newPixel.setAttribute('style', `height: ${newPixelDimension}%; width: ${newPixelDimension}%`);
         container.appendChild(newPixel);
     }
 }
 
 let userInput;
-while (userInput < 1 || userInput === undefined || userInput > 100)
-{
+while (userInput < 1 || userInput === undefined || userInput > 100) {
     userInput = prompt("What size would you like your grid to be?")
     if (userInput < 1 || userInput > 100) {
         alert("Please enter a number between 1 and 100.")
@@ -22,3 +20,12 @@ while (userInput < 1 || userInput === undefined || userInput > 100)
         break;
     }
 }
+
+let field = document.querySelector('#container');
+
+field.addEventListener('mouseover', (event) => {
+    targetedPixel = event.target;
+    targetedPixel.style.backgroundColor = 'red';
+})
+
+// Do something with event target. 
